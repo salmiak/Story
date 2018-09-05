0ef84e49799f4c02c161d2a1e25cbefb399409af
Salmiak <alfred@salmiakmedia.se>
2018-09-05 10:18:45 +0200

    Styling of carousel

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,33 +1,53 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/"><i class="fa fa-arrow-left" /></router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
+@import (css) url('https://rsms.me/inter/inter-ui.css');
+@import '../node_modules/reset-css/less/reset';
+* { box-sizing: border-box; }
+html {
+  font-size: 16px;
+  line-height: 1.5em;
+  letter-spacing: -0,004em;
+}
 body {
   padding: 0;
   margin: 0;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Inter UI', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  padding: 20px;
+  z-index: 900;
+  text-shadow: 0 0 fade(black, 30%);
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #FFF;
+    text-decoration: none;
     &.router-link-exact-active {
       color: #42b983;
     }
   }
+}
+h1 {
+  font-size: 2rem;
+  margin: 1rem;
+  font-weight: 800;
+  text-align: center;
+  color: lighten(#2c3e50, 20%);
 }
 </style>
--- client/src/views/Post.vue
+++ client/src/views/Post.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <h1>{{title}}</h1>
+    <div class="carouselContainer">
+      <carousel :perPage="1" :paginationEnabled="false" :navigationEnabled="true" @pageChange="updateCurrentImgIndex">
+        <slide v-for="img in post.images" :key="img.$index" class="imgSlide">
+          <img :src="'http://localhost:8081/image/w1024h768' + img"  />
+        </slide>
+      </carousel>
+      <div class="bgImage" v-bind:style="containerStyle" />
+    </div>
 
-    <carousel :perPage="1" :paginationEnabled="false" :navigationEnabled="true">
-      <slide v-for="img in post.images" :key="img.$index">
-        <img :src="'http://localhost:8081/image/w2048h1536' + img"  />
-      </slide>
-    </carousel>
-
-
-    <vue-markdown :watches="['post.post']" :source="post.post"></vue-markdown>
+    <div class="bodyContent">
+      <h1>{{title}}</h1>
+      <vue-markdown :watches="['post.post']" :source="post.post"></vue-markdown>
+    </div>
   </div>
 </template>
 
@@ -27,7 +30,8 @@ export default {
     return {
       post: {
         info: {}
-      }
+      },
+      currentImgIndex: 0
     }
   },
   mounted () {
@@ -44,6 +48,19 @@ export default {
         return ''
       }
       return this.post.info.Name || this.post.name
+    },
+    containerStyle () {
+      if (!this.post.name) {
+        return {}
+      }
+      return {
+        'background-image': 'url(\'http://localhost:8081/image/w1024h768' + this.post.images[this.currentImgIndex] + '\')'
+      }
+    }
+  },
+  methods: {
+    updateCurrentImgIndex (index) {
+      this.currentImgIndex = index
     }
   }
 }
@@ -51,13 +68,47 @@ export default {
 
 <style lang="less">
 img {
-  max-width: 90vw;
-  max-height: 70vh;
+  max-width: calc(100vw - 60px);
+  max-height: 100vh;
   height: auto;
   width: auto;
 }
+.carouselContainer {
+  width: 100vw;
+  background: #9AA;
+  position: relative;
+  .bgImage {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 0;
+    background-size: cover;
+    background-position: 50% 50%;
+    filter: blur(15px);
+    opacity: 0.5;
+  }
+}
 .VueCarousel {
-  width: 90vw;
+  position: relative;
+  z-index: 100;
+  width: calc(100vw - 60px);
   margin: 0 auto;
+}
+.imgSlide {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.bodyContent {
+  position: relative;
+  max-width: 600px;
+  width: 90vw;
+  margin: -25px auto 0;
+  background: #FFF;
+  border-radius: 10px;
+  z-index: 400;
+  padding: 10px 20px 30px;
 }
 </style>
